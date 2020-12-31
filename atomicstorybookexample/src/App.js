4d02835b025f073412d842271b8f804bce7bf53b
ShruthiRS-W10\Shruthi R Shetty <shruthi.shetty@robosoftin.com>
2020-12-31 20:02:05 +0530

    Initial commit

---
+++ atomicstorybookexample/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Feature from './components/organisms/feature'
+import view from "./components/templates/default/style.css";
+
+function App() {
+  return <div className={view.default}></div>;
+}
+
+export default App;
